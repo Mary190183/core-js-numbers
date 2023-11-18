7e7a9fa1811b37c73582177c78bd6147317b25fb
Mary190183 <pma190183@yandex.ru>
2023-11-18 20:56:32 +0300

    feat: toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -222,8 +222,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  let result = Number(value);
+  if (!Number.isNaN(result) && typeof result === 'number') {
+    result = +value;
+  } else result = def;
+  return result;
 }
 
 /**
